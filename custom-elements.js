854542048da5cafd99b6c7dada1070463ff6ce9c
Scott J. Miles <sjmiles@google.com>
2013-04-13 01:29:33 -0700

    update tools; modify custom-elements.js for latest loader

--- custom-elements.js
+++ custom-elements.js
@@ -7,24 +7,32 @@
 (function() {
   
 var thisFile = 'custom-elements.js';
-var scope = 'CustomElements';
-
-window[scope] = {
-  entryPointName: thisFile,
-  modules: [
+var scopeName = 'CustomElements';
+var modules = [
     'src/sidetable.js',
     'MutationObservers/MutationObserver.js',
     'src/CustomElements.js',
     'src/HTMLElementElement.js',
     'src/Parser.js'
-  ]
+];
+
+// export 
+
+window[scopeName] = {
+  entryPointName: thisFile,
+  modules: modules
 };
+
+// bootstrap
 
 var script = document.querySelector('script[src*="' + thisFile + '"]');
 var src = script.attributes.src.value;
 var basePath = src.slice(0, src.indexOf(thisFile));
 
-document.write('<script src="' + basePath + '/tools/loader/loader.js" scope="' 
-  + scope + '"></script>');
+if (!window.Loader) {
+  var path = basePath + '/tools/loader/loader.js';
+  document.write('<script src="' + path + '"></script>');
+} 
+document.write('<script>Loader.load("' + scopeName + '")</script>');
   
 })();
