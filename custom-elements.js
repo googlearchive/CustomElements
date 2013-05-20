8ba61d445c6ec1c9a28aaa1f09714e6d61ee44c4
Wesley Cho <wesley.cho@gmail.com>
2013-05-20 15:58:55 -0400

    Corrected MutationObserver.js location

--- custom-elements.js
+++ custom-elements.js
@@ -10,7 +10,7 @@ var thisFile = 'custom-elements.js';
 var scopeName = 'CustomElements';
 var modules = [
     'src/sidetable.js',
-    'MutationObservers/MutationObserver.js',
+    'src/MutationObserver.js',
     'src/CustomElements.js',
     'src/MutationObserver.js',
     'src/Observer.js',
