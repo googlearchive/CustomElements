3ef94da4e027d5a85402db188ee7da422b176df9
Steve Orvell <steve@orvellia.com>
2013-08-27 08:27:37 -0700

    fire WebComponentsReady event without delay

--- src/boot.js
+++ src/boot.js
@@ -8,25 +8,22 @@
 // bootstrap parsing
 
 function bootstrap() {
-  // go async so call stack can unwind
-  setTimeout(function() {
-    // parse document
-    CustomElements.parser.parse(document);
-    // one more pass before register is 'live'
-    CustomElements.upgradeDocument(document);  
-    // set internal 'ready' flag, now document.register will trigger 
-    // synchronous upgrades
-    CustomElements.ready = true;
-    // capture blunt profiling data
-    CustomElements.readyTime = Date.now();
-    if (window.HTMLImports) {
-      CustomElements.elapsed = CustomElements.readyTime - HTMLImports.readyTime;
-    }
-    // notify the system that we are bootstrapped
-    document.body.dispatchEvent(
-      new CustomEvent('WebComponentsReady', {bubbles: true})
-    );
-  }, 0);
+  // parse document
+  CustomElements.parser.parse(document);
+  // one more pass before register is 'live'
+  CustomElements.upgradeDocument(document);  
+  // set internal 'ready' flag, now document.register will trigger 
+  // synchronous upgrades
+  CustomElements.ready = true;
+  // capture blunt profiling data
+  CustomElements.readyTime = Date.now();
+  if (window.HTMLImports) {
+    CustomElements.elapsed = CustomElements.readyTime - HTMLImports.readyTime;
+  }
+  // notify the system that we are bootstrapped
+  document.body.dispatchEvent(
+    new CustomEvent('WebComponentsReady', {bubbles: true})
+  );
 }
 
 // CustomEvent shim for IE
